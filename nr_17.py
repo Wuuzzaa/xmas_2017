c6fea642b44969f1f1533c5913cd192af5e34418
Wuuzzaa <jonas-licht@gmx.de>
2018-01-07 16:46:39 +0100

    nr_17 gelöst

--- nr_17.py
+++ nr_17.py
@@ -1,31 +1,28 @@
-laenge = 3
-zahlen = [x for x in range(1, laenge + 1)]
+# Die ersten Lösungen sind in der Aufgabe gegeben
+# Jeder Anzahl von Elementen in einer Teilliste wird die Anzahl der Möglichen Kombinationen zugeteilt
+# So gibt es bei 4 Elementen in einer Liste 5 zulässige Möglichkeiten
+# Um die Lösung von n zu berechnen muss das Ergebnis von n-1 bekannt sein
+loesungen = {1: 1,
+             2: 1,
+             3: 2,
+             4: 5}
 
+# Nacheinander die Lösungen von 5 bis 21 berechnen
+for i in range(5, 22):
+    laenge = i
+    zahlen = [x for x in range(1, laenge + 1)]
 
-def berechne_klammerpaare(linke_klammer, rechte_klammer):
-    #print("linke klammer {}".format(linke_klammer))
-    #print("rechte klammer {}".format(rechte_klammer))
-    global ergebnisse
+    def berechne_moeglichkeiten(l, r):
+        # Die Anzahl der gesamten Möglichkeiten entspricht der Anzahl der Möglichkeiten der linken Klammer
+        #  mal der Anzahl der Möglichkeiten der rechten Klammer
+        return loesungen[len(l)] * loesungen[len(r)]
 
-    if linke_klammer and rechte_klammer:
-        ergebnisse += 1
+    moeglichkeiten = 0
 
-    if len(linke_klammer) > 2:
-        #return berechne_klammerpaare(linke_klammer[1:], rechte_klammer)
-        for x in range(1, len(linke_klammer) ):
-            return berechne_klammerpaare(linke_klammer[:x], linke_klammer[x:])
+    # Erzeugt die Elemente der Linken und Rechten Klammer
+    for x in range(1, len(zahlen)):
+        moeglichkeiten += berechne_moeglichkeiten(zahlen[:x], zahlen[x:])
+    loesungen[laenge] = moeglichkeiten
 
-    if len(rechte_klammer) > 2:
-        #return berechne_klammerpaare(linke_klammer, rechte_klammer[1:])
-        for x in range(1, len(rechte_klammer) ):
-            return berechne_klammerpaare(rechte_klammer[:x], rechte_klammer[x:])
+print("Es gibt {} Möglichkeiten 20 nicht triviale Klammerpaare einzufügen".format(loesungen[21]))
 
-global ergebnisse
-ergebnisse = 0
-
-for x in range(1, len(zahlen)):
-    print("linke klammer {}".format(zahlen[:x]))
-    print("rechte klammer {}".format(zahlen[x:]))
-    berechne_klammerpaare(zahlen[:x], zahlen[x:])
-
-print(ergebnisse)
